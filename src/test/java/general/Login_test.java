ad4a8b335a499b09a7ee29b0ac48fdb5077b89ee
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2023-12-21 15:18:40 +0530

    magic link send

--- src/test/java/general/Login_test.java
+++ src/test/java/general/Login_test.java
@@ -91,7 +91,6 @@ public class Login_test
 					ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='OK']")));
 			driver.findElement(By.xpath("//android.widget.TextView[@text='OK']")).click();
 
-
 			driver.closeApp();
 
 			// // Open Gmail
